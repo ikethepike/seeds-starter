74b748682482f734fa101be13c30362c84d7a3e6
ikethepike <ikethepike@outlook.com>
2024-02-28 19:11:44 +0100

    Changes

--- src/components/views/NowPlaying.view.tsx
+++ src/components/views/NowPlaying.view.tsx
@@ -1,15 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import AlbumArt from "../AlbumArt";
 import ProgressBar from "../ProgressBar";
 
-let cheat = 0;
+import "./_NowPlaying.css";
 
 export default function NowPlaying() {
   const [progress, setProgress] = useState(0);
+  const audio = useRef(
+    new Audio("http://localhost:3000/music/public-domain/pop.mp3")
+  );
 
   return (
     <section className="now-playing">
-      <AlbumArt src="https://placebear.com/500/500" alt="Kitty cat" />
+      <AlbumArt
+        src="https://f4.bcbits.com/img/a4058062868_5.jpg"
+        alt="Kitty cat"
+      />
       <ProgressBar progressPercent={50} />
     </section>
   );
