b18d025c6a26850d6bdaec73b651154f0aa4a1ba
ikethepike <ikethepike@outlook.com>
2024-03-06 17:58:58 +0100

    Update nowplaying

--- src/components/views/NowPlaying.view.tsx
+++ src/components/views/NowPlaying.view.tsx
@@ -7,8 +7,15 @@ import "./_NowPlaying.css";
 export default function NowPlaying() {
   const [progress, setProgress] = useState(0);
   const audio = useRef(
-    new Audio("http://localhost:3000/music/public-domain/pop.mp3")
+    new Audio("http://localhost:3000/music/public-domain/blues.mp3")
   );
+
+  useEffect(() => {
+    audio.current.addEventListener("timeupdate", () => {
+      const progress = audio.current.currentTime / audio.current.duration;
+      setProgress(progress * 100);
+    });
+  }, []);
 
   return (
     <section className="now-playing">
@@ -16,7 +23,15 @@ export default function NowPlaying() {
         src="https://f4.bcbits.com/img/a4058062868_5.jpg"
         alt="Kitty cat"
       />
-      <ProgressBar progressPercent={50} />
+      <ProgressBar progressPercent={progress} />
+
+      <button
+        onClick={() => {
+          audio.current.play();
+        }}
+      >
+        Play
+      </button>
     </section>
   );
 }
